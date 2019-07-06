546f9da630baeffc9f952747ef61d5e15c8e724e
Cameron O'Rourke <corourke@Cameron-Kinetica-MBP.local>
2019-07-06 13:41:23 -0700

    Sync w/tutorial

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import OtherPage from './OtherPage';
+import App from './App';
 
-it('does not have any tests yet', () => {
-});
+it('renders without crashing', () => {});
